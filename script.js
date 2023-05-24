4d034c4c4b9463f362f7e62fa9bc30b4371dc592
Adrak <38663765+adrak0@users.noreply.github.com>
2023-05-24 19:31:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add your custom JavaScript code here
+// You can add functionality like handling form submissions, dynamic content loading, etc.
+// Example code:
+/*
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  // Process form submission here
+});
+*/
+
+// You can add more JavaScript code based on your specific needs
