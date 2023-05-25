3fdce959e36af1e5232b90f723af1d7f7055d346
Adrak <38663765+adrak0@users.noreply.github.com>
2023-05-25 10:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-// Add your custom JavaScript code here
-// You can add functionality like handling form submissions, dynamic content loading, etc.
-// Example code:
-/*
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  // Process form submission here
-});
-*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
 
-// You can add more JavaScript code based on your specific needs
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Toggle navigation menu on mobile devices
+document.querySelector('nav ul').addEventListener('click', function() {
+  this.classList.toggle('active');
+});
+
+// Add class to header on scroll
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// Customize game trailer section
+const gameTrailer = document.getElementById('game-trailer');
+gameTrailer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+// Customize screenshot showcase section
+const screenshotShowcase = document.getElementById('screenshot-showcase');
+for (let i = 1; i <= 6; i++) {
+  const screenshot = document.createElement('img');
+  screenshot.src = `screenshots/screenshot${i}.jpg`;
+  screenshot.alt = `Screenshot ${i}`;
+  screenshotShowcase.appendChild(screenshot);
+}
+
+// Customize concept art section
+const conceptArt = document.getElementById('concept-art');
+for (let i = 1; i <= 4; i++) {
+  const artPiece = document.createElement('img');
+  artPiece.src = `concept-art/art${i}.jpg`;
+  artPiece.alt = `Concept Art ${i}`;
+  conceptArt.appendChild(artPiece);
+}
