550263715268cadfe10e653059d8929dc2463ef1
Adrak <38663765+adrak0@users.noreply.github.com>
2023-06-26 11:20:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,3 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scrolled');
   }
 });
-
-// Customize game trailer section
-const gameTrailer = document.getElementById('game-trailer');
-gameTrailer.innerHTML = `<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/frYuzf3N9Vs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
