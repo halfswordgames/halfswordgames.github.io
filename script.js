ba7854c73a7dfbbb18e2dba7465dc4cb81a35643
adrak0 <38663765+adrak0@users.noreply.github.com>
2023-10-09 12:28:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Adjusted scrollIntoView options for better mobile view
-    // Close the navigation menu on mobile devices after clicking a link
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     const nav = document.querySelector('nav ul');
     nav.classList.remove('active');
   });
 });
 
-// Toggle navigation menu on mobile devices
 document.querySelector('nav ul').addEventListener('click', function() {
   this.classList.toggle('active');
-});
-
-// Add class to header on scroll
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
-  if (window.scrollY > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
 });
